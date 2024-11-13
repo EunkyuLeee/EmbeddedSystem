4207a5eda3ef68369f2d98326ede42c1e4e6597e
Eunkyu Lee <lek7146@gmail.com>
2024-11-13 15:57:57 +0900

    init spring project

---
+++ smartfarm/src/test/java/kr/kro/es7/smartfarm/SmartfarmApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.kro.es7.smartfarm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartfarmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
